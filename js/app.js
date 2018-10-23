677c4ec20ef52b4bbc8a45604039a0c3cdca88c9
Vladimir Poliatykin <vladimir.poliatykin@gmail.com>
2018-10-23 17:56:09 +0300

    introduced routing

--- js/app.js
+++ js/app.js
@@ -1,96 +1,88 @@
 /**
  * You must include the dependency on 'ngMaterial'
  */
-var myApp = angular.module('MyApp', ['ngMaterial', 'ngMessages']);
-myApp.controller('productsController',
-    function($scope, $http){
+var myApp = angular.module('MyApp', ['ngMaterial', 'ngMessages', 'ngRoute']);
+myApp.controller('productsController', function productsController($scope, $http, $location, $templateCache, $routeParams){
 
-        $http({method: 'GET', url: 'products.json'}).
-        then(function success(response) {
-            $scope.items = response.data;
-            $scope.categoriesList = new Set();
-            $scope.products = $scope.items.products;
+    $scope.$on('$routeChangeStart', function(event, next, current) {
+        if (typeof(current) !== 'undefined'){
+            $templateCache.remove(next.templateUrl);
+        }
+        $scope.$on("$routeChangeSuccess", function () {
 
-            for(let elem of $scope.products){
-                $scope.categoriesList.add(elem.bsr_category);
-            }
+            var category = $routeParams["category"];
 
-            $scope.catrs = [];
-            for(let elem of $scope.categoriesList){
-                $scope.catrs.push(elem);
+            if(category!=='undefined'){
 
-            }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        });
-
-        angular.element(document).ready(function () {
-
-
-            // Your document is ready, place your code here
-            // $scope.products = angular.element(document.querySelector('.products-container'));
-            // $scope.categories = angular.element(document.querySelector('.categories'));
-            // $scope.productsWidth = $scope.products[0].offsetTop;
-            // $scope.categories.css('top',  $scope.productsWidth);
-
-
-
-
-            $scope.categoriesListItems = angular.element(document.querySelectorAll('.categories-item'));
-            $scope.renderedItems = angular.element(document.querySelectorAll('.item-box'));
-
-            $scope.productsWrapper = angular.element(document.querySelector('.products'));
-            console.log($scope.productsWrapper);
-            $scope.categoriesListItems[0].onclick =  function(e){
-                for(var i = 0; i < $scope.renderedItems.length; i++){
-
-                        $scope.productsWrapper.append($scope.renderedItems[i]);
-
-                };
-            };
-            for(var i = 1 ; i < $scope.categoriesListItems.length; i++){
-                $scope.categoriesListItems[i].onclick = function(e){
-
-                    $scope.categorySetItems = [];
-
-                    for(var i = 0; i < $scope.renderedItems.length; i++){
-                        if($scope.renderedItems[i].dataset.category == this.dataset.category){
-                            $scope.categorySetItems.push($scope.renderedItems[i]);
+                $http({method: 'GET', url: 'products.json', params: {category: category}}).
+                then(function success(response) {
+                    $scope.data = response.data;
+                    $scope.items = $scope.data;
+                    console.log(category);
+                    $scope.categoriesList = new Set();
+                    $scope.products = $scope.items.products;
+                    for(let elem of $scope.products){
+                        $scope.categoriesList.add(elem.bsr_category);
+                    }
+                   $scope.itemsList = [];
+                    for(let key of $scope.items.products){
+                        if(key.bsr_category == category){
+                            $scope.itemsList.push(key);
                         }
-                            $scope.renderedItems[i].remove();
-
                     }
+                    $scope.items =  (category!= 'all')? $scope.itemsList : $scope.data.products;
 
-                    for(let i = 0 ; i < $scope.categorySetItems.length; i++){
-                        $scope.productsWrapper.append($scope.categorySetItems[i]);
+                    $scope.catrs = [];
+                    for(let elem of $scope.categoriesList){
+                        $scope.catrs.push(elem);
                     }
+                });
+
+                angular.element(document).ready(function () {
+                    $scope.categoriesListItems = angular.element(document.querySelectorAll('.categories-link-item'));
+                    $scope.renderedItems = angular.element(document.querySelectorAll('.item-box'));
+                    $scope.productsWrapper = angular.element(document.querySelector('.products'));
+
+                    $scope.categoriesSetofArrays = new Set();
+                    // $scope.categoriesListItems[0].onclick =  function(e){  ///Show all products when press 'All categories'
+                    //
+                    //     for(var i = 0; i < $scope.renderedItems.length; i++){
+                    //
+                    //         $scope.productsWrapper.append($scope.renderedItems[i]);
+                    //     }
+                    //
+                    // };
 
 
 
+                });
 
-
-                };
-            };
-
+            }
         });
 
 
+    });
+}
 
-
-    }
 
 );
+myApp.config(function($routeProvider){
+    $routeProvider
+
+        .when('/',
+            {
+                templateUrl: 'js/view/productsView.html',
+                controller: 'productsController',
+            });
+    $routeProvider
+        .when(`/prods/:category`,
+            {
+                templateUrl: 'js/view/productsView.html',
+                controller: 'productsController',
+            });
+
+
+
+    $routeProvider.otherwise({redirectTo: '/'});
+
+});
