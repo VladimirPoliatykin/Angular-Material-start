68706436d4d08b62508435fca8d862c926f59900
Vladimir Poliatykin <vladimir.poliatykin@gmail.com>
2018-10-25 13:59:11 +0300

    semi working routing with two arguments

--- js/app.js
+++ js/app.js
@@ -2,21 +2,38 @@
  * You must include the dependency on 'ngMaterial'
  */
 angular.module('MyApp', ['ngMaterial', 'ngMessages', 'ngRoute'])
-    .controller('productsController', function productsController($scope, $http, $location, $templateCache, $routeParams){
+    .controller('productsController', function productsController($scope, $http, $templateCache, $routeParams){
+            $scope.searchValueFromField = localStorage.getItem('value');
+            var searchField = angular.element(document.querySelector('.search-field'));
 
+            // searchField.on('input', function(e){
+            //     $scope.searchValueFromField = searchField.val() || ''; *
 
-     $scope.$on('$routeChangeStart', function(event, next, current) {
-        if (typeof(current) !== 'undefined'){
-            $templateCache.remove(next.templateUrl);
-        }
+            // });
+            localStorage.setItem('value',$scope.searchValueFromField);
+            
+            $scope.renderBox = angular.element(document.querySelector('.item-box'));
+
+            $scope.$on('$routeChangeStart', function(event, next, current) {
+                $scope.searchValueFromField = searchField.val() || '';
+
+                if (typeof(current) !== 'undefined') {
+                 $templateCache.remove(next.templateUrl);
+             }
+        $scope.$on('$$routeUpdate', function(){
+        });
+
         $scope.$on("$routeChangeSuccess", function () {
+
             var category = $routeParams["category"];
             var searchWord = $routeParams['searchWord'];
-            $http({method: 'GET', url: 'products.json', params: {category: category,
-                    searchWord: $scope.searchedValueFromField}}).
-                then(function success(response) {
-                $scope.data = response.data;
+            searchField.val($routeParams.searchWord);
 
+            $http({method: 'GET', url: 'products.json', params: {category: category,
+                    searchWord: searchWord}}).
+                then(function success(response) {
+
+                $scope.data = response.data;
                 (function getCategoryList(){
                     $scope.categoriesList = new Set();
                     $scope.products = $scope.data.products;
@@ -27,7 +44,8 @@ angular.module('MyApp', ['ngMaterial', 'ngMessages', 'ngRoute'])
 
                     if(category === undefined && searchWord == undefined){
                         $scope.items = $scope.data.products;
-                    }if(category !== undefined  && searchWord !== undefined){
+                    }if(category !== undefined  || searchWord !== undefined){
+
                         (function choseCurrentCategory(){
                             $scope.itemsList = [];
                             for(var key of $scope.products){
@@ -37,6 +55,7 @@ angular.module('MyApp', ['ngMaterial', 'ngMessages', 'ngRoute'])
                             }
                         })();
                         $scope.items =  (category!= 'all')? $scope.itemsList : $scope.data.products;
+
                     }
                         $scope.catrs = [];
                         for(let elem of $scope.categoriesList){
@@ -53,14 +72,7 @@ angular.module('MyApp', ['ngMaterial', 'ngMessages', 'ngRoute'])
 )
 .config(function($routeProvider){
     $routeProvider
-
         .when('/',
-            {
-                templateUrl: 'js/view/productsView.html',
-                controller: 'productsController',
-            });
-    $routeProvider
-        .when(`/prods/:category/`,
             {
                 templateUrl: 'js/view/productsView.html',
                 controller: 'productsController',
